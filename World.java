7d0b479a5a0c385ba6853bb3d955a1f4072f40cb
“ssoeun-y” <ssoeun.yy@gmail.com>
2025-04-23 09:21:50 +0900

    five

--- World.java
+++ World.java
@@ -3,5 +3,8 @@ public class World {
     public static void run() {
       System.out.println("202214151");
     }
+    public static void run2() {
+        System.out.println("hello world");
+      }    
   }
   
